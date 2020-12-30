02f51e668fa64d6a2345ff2cdee63c84bcc93670
timotheemcdonald <timotheemcdonald@gmail.com>
2020-12-29 21:39:12 -0500

    working database

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 import express from 'express'
 import mongoose from 'mongoose'
+import Cors from 'cors'
+
+import Cards from './dbCards.js'
 
 // App Config
 const app = express();
@@ -7,6 +10,8 @@ const port = process.env.PORT || 8001
 const connection_url = `mongodb+srv://admin:657dDjTvsj73FJDD@cluster0.4497d.mongodb.net/tinderdb?retryWrites=true&w=majority`
 
 // Middleware
+app.use(express.json());
+app.use(Cors());
 
 // DB config
 mongoose.connect(connection_url, {
@@ -18,6 +23,28 @@ mongoose.connect(connection_url, {
 // API end points
 app.get('/', (req, res) => {
     res.status(200).send('HELLO WORLD');
+})
+
+app.post('/tinder/cards', (req, res) => {
+    const dbCard = req.body;
+
+    Cards.create(dbCard, (err, data) => {
+        if(err){
+            res.status(500).send(err);
+        }else{
+            res.status(201).send(data);
+        }
+    })
+})
+
+app.get('/tinder/cards', (req, res) => {
+    Cards.find((err, data) => {
+        if(err){
+            res.status(500).send(err);
+        } else {
+            res.status(200).send(data);
+        }
+    })
 })
 
 // Listener
